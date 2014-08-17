53d777f93a39d6fecc22bbb6c17f20af586c5960
Michael van der Weg <michael@joinbox.com>
2014-08-17 21:34:58 +0200

    fixed ttl engine

--- test.js
+++ test.js
@@ -1,17 +1,24 @@
 
 
 
-	var LRUCache = require( "./" );
+	var LRUCache = require('./');
 
 
 
 
-	var cache = new LRUCache();
+	var cache = new LRUCache({
+        //ttl: 100
+        //, forceTtl: true
+    });
 
 
-	cache.set( "a", 11 );
-	cache.set( "b", 22 );
-	cache.set( "a", 33 );
+	cache.set('a', 11, 100);
+	cache.set('b', 22);
+	cache.set('a', 33);
 
-	console.log( cache.get( "a" ) );
-	console.log( cache.length );
+	console.log(cache.get('a'));
+	console.log(cache.length);
+
+    setTimeout(function() {
+        console.log(cache.get('a'));
+    }, 12000);
